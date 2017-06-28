41230ae23b01fe5ea1c46a4911fd5214a832c11a
nisharg patel <patelnisharg8@gmail.com>
2017-06-28 07:39:40 -0400

    calculator UI and VC setup

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by nisharg patel on 2017-06-27.
+//  Copyright © 2017 nisharg patel. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by nisharg patel on 2017-06-26.
+//  Copyright © 2017 nisharg patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    @IBOutlet weak var operationDescription: UILabel!
+
+
+    @IBOutlet weak var display: UILabel!
+    
+    
+    
+    
+    @IBAction func digit(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    @IBAction func operand(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
